7152de0704f5d98d43866f1cf22b863fedcc9115
Danielthefreedomlover <daniyalarifthepro@gmail.com>
2025-10-05 16:21:49 +0530

    Update inversefun.lua

--- inversefun.lua
+++ inversefun.lua
@@ -1,4 +1,5 @@
-kilometers = io.read("n*")
+io.write("Enter the kilometers you want to convert:")
+kilometers = io.read("n")
 function inversefunc()
 doinversefunc = kilometers / 1.609
 return kilometers / 1.609
